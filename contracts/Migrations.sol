ce85bf7d8ac0a06c585467ae3a3bc14d015bada1
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2024-05-24 18:27:48 -0600

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -5,13 +5,13 @@ contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
-  modifier restricted() {
+ /* modifier restricted() {
     require(
       msg.sender == owner,
       "This function is restricted to the contract's owner"
     );
     _;
-  }
+  }  */
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
